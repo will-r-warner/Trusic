2ea1cfbf7a89e0bf5392560e7c3c3f3fead75667
William Warner <will.r.warner@gmail.com>
2020-01-13 22:29:19 -0600

    Initial layout for home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { HomeComponent } from "./components/home/home.component";
+import { HomeCityComponent } from "./components/home/home-city/home-city.component";
+import { FooterIconComponent } from "./components/footer/footer-icon/footer-icon.component";
+import { HomeFeaturedArtistsComponent } from './components/home/home-featured-artists/home-featured-artists.component';
+import { HomeUpcomingShowsComponent } from './components/home/home-upcoming-shows/home-upcoming-shows.component';
+import { HomeUpcomingShowCardComponent } from './components/home/home-upcoming-show-card/home-upcoming-show-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    HomeCityComponent,
+    FooterIconComponent,
+    HomeFeaturedArtistsComponent,
+    HomeUpcomingShowsComponent,
+    HomeUpcomingShowCardComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
