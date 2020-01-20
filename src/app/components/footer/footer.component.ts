46fd4b06af869f14222449e89d3aff9122c412c6
William Warner <will.r.warner@gamil.com>
2020-01-20 13:07:41 -0600

    Update trusic-logo

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -9,7 +9,7 @@ export class FooterComponent implements OnInit {
   icons = [
     { src: "assets/icons/home-solid.svg", alt: "Home icon" },
     { src: "assets/icons/star-regular.svg", alt: "Star icon" },
-    { src: "assets/icons/trusic-logo.png", alt: "Trusic logo" },
+    { src: "assets/icons/trusic-logo.svg", alt: "Trusic logo" },
     { src: "assets/icons/map-marker-alt-solid.svg", alt: "Map icon" },
     { src: "assets/icons/list-alt-regular.svg", alt: "List icon" }
   ];
